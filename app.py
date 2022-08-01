d8358ea784d3bd8ca46d99acefa98e919a5074d6
Arthur Salgado <arthursaalgado@gmail.com>
2022-08-01 17:25:14 -0300

    fix'd spelling

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ class MyClient(discord.Client):
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encondig='utf-8', mode='w')
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
