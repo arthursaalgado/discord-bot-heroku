412565148efb688ec895830840844fede3c0e0e3
Arthur Salgado <arthursaalgado@gmail.com>
2022-08-01 17:21:44 -0300

    logging

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import discord
+import logging
 
 
 with open('token', 'r') as file:
     TOKEN = file.read()
-
 
 class MyClient(discord.Client):
     
@@ -13,6 +13,11 @@ class MyClient(discord.Client):
     async def on_message(self):
         print(f'Message from {self.author}: {self.content}')
 
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encondig='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 client = MyClient()
 client.run(TOKEN)
