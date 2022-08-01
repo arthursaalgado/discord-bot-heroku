c7b90853bc61d88f20038b29b3f208aa24098968
Arthur Salgado <arthursaalgado@gmail.com>
2022-08-01 16:45:11 -0300

    first bot implementation

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 import discord
 
-while True:
-    print('Oi')
+class MyClient(discord.Client):
+    async def on_ready():
+        print(f'Logged on as {self.user}')
+    
+    async def on_message():
+        print(f'Message from {self.author}: {self.content}')
 
+
+client = MyClient()
+client.run('7270b5127502d5e40f1b6d1d7fa0d1b105f696745ad66ce62dc2f19be690229d')
