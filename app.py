1867b91ebcf4cfc844e706c1afdfc5169bc895a8
Arthur Salgado <arthursaalgado@gmail.com>
2022-08-02 14:06:55 -0300

    ?

--- app.py
+++ app.py
@@ -1,21 +1,15 @@
 import asyncio
-import logging
 import os
 
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix = '!')
+bot = commands.Bot(command_prefix='!')
 
 @bot.event
-async def on_ready(ctx):
-    print(f'{bot.user.name} is online!\n Currently connect to:')
+async def on_ready():
+    print( f'{bot.user.name} has connected to Discord on server!\nCurrently connected to: ')
     async for guild in bot.fetch_guilds():
         print('- '+guild.name)
 
-logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-logger.addHandler(handler)
 
 bot.run(os.environ['TOKEN_KEY'])
