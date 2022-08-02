37f2f5975b8f86cc9dc8637518d3374a52a6713f
Arthur Salgado <arthursaalgado@gmail.com>
2022-08-02 13:54:35 -0300

    app.py refactored

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
-import discord
+import asyncio
 import logging
 import os
 
+from discord.ext import commands
 
-class MyClient(discord.Client):
-    
-    async def on_ready(self):
-        print(f'Logged on as {self.user}')
-    
-    async def on_message(self, message):
-        print(f'Message from {self.author}: {self.content}')
+client = commands.Bot(command_prefix = '!')
 
-    
+@bot.event
+async def on_ready():
+    print(f'{client.user.name} is online!\n Currently connect to:')
+    async for guild in client.fetch_guilds():
+        print('- '+guild.name)
+
+
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
@@ -19,5 +20,4 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-client = MyClient()
 client.run(os.environ['TOKEN_KEY'])
