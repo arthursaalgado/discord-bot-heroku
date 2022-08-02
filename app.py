45917fcadadc1bc02761c38c085f06f3d1d8f9a4
Arthur Salgado <arthursaalgado@gmail.com>
2022-08-02 14:10:32 -0300

    on_message event

--- app.py
+++ app.py
@@ -7,9 +7,17 @@ bot = commands.Bot(command_prefix='!')
 
 @bot.event
 async def on_ready():
-    print( f'{bot.user.name} has connected to Discord on server!\nCurrently connected to: ')
+    print( f'{bot.user.name} in online!\nCurrently connected to: ')
     async for guild in bot.fetch_guilds():
         print('- '+guild.name)
 
+@bot.event
+async def on_message(ctx):
+	print(
+		ctx.message.author,
+		ctx.message.text,
+		ctx.guild,
+	)
+	await ctx.send('salve')
 
 bot.run(os.environ['TOKEN_KEY'])
