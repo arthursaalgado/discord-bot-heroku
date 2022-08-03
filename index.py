4233961364d8c98e70f9e3a061845d946faf644e
Arthur Salgado <arthursaalgado@gmail.com>
2022-08-03 17:41:47 -0300

    .

--- index.py
+++ index.py
@@ -7,13 +7,13 @@ class Music(commands.Cog):
     def __init__(self, bot):
         self.bot=bot
 
-class Client(commands.Bot):
+class MyClient(commands.Cog):
+    @commands.Cog.listener()
     async def on_ready():
         print(f'Logged in as {bot.user} (ID: {bot.user.id})')
         print('~~~~~~~~~~')
 
 intents = discord.Intents.default()
-
 bot = commands.Bot(
     command_prefix='!',
     intents = intents
@@ -22,7 +22,7 @@ bot = commands.Bot(
 async def main():
     async with bot:
         await bot.add_cog(Music(bot))
-        await bot.add_cog(Client())
+        await bot.add_cog(Client(bot))
         await bot.start(os.environ['TOKEN'])
 
 asyncio.run(main())
